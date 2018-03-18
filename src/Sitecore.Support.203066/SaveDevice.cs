8d2b2bef842a3c5031a3a6a20bb76cbb9a35af0d
Sergey Plashenko <spl@sitecore.net>
2018-03-18 10:12:33 +0200

    Code from Reflector

---
+++ src/Sitecore.Support.203066/SaveDevice.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Analytics.DataAccess.Dictionaries;
+using Sitecore.Diagnostics;
+using System;
+
+namespace Sitecore.Analytics.Pipelines.SubmitSessionContext
+{
+  internal class SaveDevice : SubmitSessionContextProcessor
+  {
+    public override void Process(SubmitSessionContextArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      bool flag = args.Session.Device != null;
+      if (flag)
+      {
+        KnownDataDictionaries.Devices.Put(args.Session.Device);
+      }
+    }
+  }
+}
